b47f95a22f9674194409cedceecc1d3c9773e832
Farrhouq <farouqimoro@gmail.com>
2024-05-21 15:31:58 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
-# Install flask
+# This puppet manifest is used to install a package
+
 package { 'python3-pip':
   ensure => installed,
 }
 
-package {'Flask':
-  ensure => '2.1.0',
+package { 'Flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
-  require => Package['python3-pip'],
+  require  => Package['python3-pip'],
 }
